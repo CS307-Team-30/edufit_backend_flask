d7bfb16223a57a7f80a3481eb2673111d9228681
jhcheng01 <jhcheng@purdue.edu>
2023-11-21 10:15:28 -0600

    Additional merge

--- server/app.py
+++ server/app.py
@@ -381,6 +381,37 @@ def login_user():
         "token": token
     })
 
+@app.route("/get-profile", methods=["GET"])
+def get_profile():
+    print("Starting...")
+
+    token = request.args.get("authToken")
+    token_data = jwt.decode(token, secret_key, algorithms=["HS256"])
+
+    # print("Got token: " + token)
+
+    if token is None:
+        print("Token is invalid")
+        return jsonify({"error": "Token is invalid"}), 401
+
+    user_id = token_data['id']
+    # print("user_id" + user_id)
+
+    user_id = request.args.get('user_id')
+    profile = Profile.query.filter_by(user_id=user_id).first()
+
+    if profile is None:
+        print("Profile does not exist")
+        return jsonify({"error": "Profile does not exist"}), 401
+        return jsonify({"error": "Profile does not exist"}), 404
+
+    return jsonify({
+        "user_id": profile.user_id,
+        "nickname": profile.nickname,
+        "bio": profile.bio,
+        "profile_pic": profile.profile_pic
+    })
+
 @app.route("/update_profile", methods=["POST"])
 def update_profile():
 
@@ -480,6 +511,7 @@ def send_message():
     db.session.commit()
     # Ask Arnob if this needs to return anything
     return jsonify({"msg": "Message sent!"}), 200
+
 @app.route("/get-messages", methods=["GET"])
 def get_messages():
     
