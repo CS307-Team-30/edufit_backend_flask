2156ea893f4bd18abae8094cdb17e0481041dd46
Arnob Kazi <shahirmuktasid22@gmail.com>
2023-11-21 00:33:15 -0500

    merge resolution

--- server/models.py
+++ server/models.py
@@ -112,19 +112,3 @@ class User(db.Model):
 
 
     posts = relationship("Post", back_populates="user")
-
-# cs_59300 = Community(id=1, name="CS 59300 - Special Topics")
-# cs_59799 = Community(id=2, name="CS 59799 - Graduate Professional Practice")
-# cs_15900 = Community(id=3, name="CS 15900 - C Programming")
-# cs_18000 = Community(id=4, name="CS 18000 - Problem Solving And Object-Oriented Programming")
-# cs_18200 = Community(id=5, name="CS 18200 - Foundations Of Computer Science")
-# cs_18300 = Community(id=6, name="CS 18300 - Professional Practice I")
-# cs_18400 = Community(id=7, name="CS 18400 - Professional Practice II")
-# cs_19000 = Community(id=8, name="CS 19000 - Topics In Computer Sciences")
-# cs_50011 = Community(id=9, name="CS 50011 - Introduction To Systems For Information Security")
-# cs_17600 = Community(id=10, name="CS 17600 - Data Engineering In Python")
-# cs_17700 = Community(id=11, name="CS 17700 - Programming With Multimedia Objects")
-
-# # Add these instances to the session and commit to save changes
-# db.session.add_all([cs_59300, cs_59799, cs_15900, cs_18000, cs_18200, cs_18300, cs_18400, cs_19000, cs_50011, cs_17600, cs_17700])
-# db.session.commit()
